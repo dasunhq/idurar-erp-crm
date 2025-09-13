e01b0a4cec28574e15515ab6dd9090f56172baff
h4s1nth4 <jmhdilsha@gmail.com>
2025-09-13 20:02:02 +0530

    fix: added reports and configs

--- mongo-init.js
+++ mongo-init.js
@@ -2,9 +2,14 @@
 db = db.getSiblingDB("idurar_db");
 
 // Create user for the application
+// Note: Password should be set via environment variable MONGO_INITDB_ROOT_PASSWORD
+const adminPassword = process.env.MONGO_ADMIN_PASSWORD || (() => {
+  throw new Error("MONGO_ADMIN_PASSWORD environment variable is required");
+})();
+
 db.createUser({
-  user: "admin@gmail.com",
-  pwd: "admin123",
+  user: process.env.MONGO_ADMIN_USER || "admin@gmail.com",
+  pwd: adminPassword,
   roles: [
     {
       role: "readWrite",
