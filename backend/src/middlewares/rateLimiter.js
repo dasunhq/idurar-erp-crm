05385a7eba304d739bf1f2e527530d2e145d5e6e
h4s1nth4 <jmhdilsha@gmail.com>
2025-09-13 19:15:02 +0530

    fix: added reports and configs

--- backend/src/middlewares/rateLimiter.js
+++ backend/src/middlewares/rateLimiter.js
@@ -6,7 +6,7 @@ const rateLimit = require('express-rate-limit');
  */
 const apiLimiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 10000, // Limit each IP to 100 requests per windowMs
+  max: 100, // Limit each IP to 100 requests per windowMs
   message: {
     success: false,
     result: null,
